f905eabdac68f3331addf46306bffa02b4ce07a9
palvo <palvo@DESKTOP-INFG1MU>
2022-09-29 18:16:11 +0200

    initialize master branch

---
+++ PlatformerTutorial/src/main/GameWindow.java
@@ -0,0 +1,20 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class GameWindow {
+	
+	private JFrame jFrame;
+	
+	public GameWindow (GamePanel gamePanel) {
+		jFrame = new JFrame();
+		jFrame.setSize(400, 400);
+		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jFrame.setLocationRelativeTo(null);
+		
+		jFrame.add(gamePanel);
+		
+		jFrame.setVisible(true);
+	}
+
+}
