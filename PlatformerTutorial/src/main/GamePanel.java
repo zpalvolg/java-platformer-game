087897a5c5e32df356b241f83a14f56e9afda99c
zpalvolg <zpalvolg@zpalvolg-ThinkCentre-M82>
2022-09-29 20:56:41 +0200

    add player image

--- PlatformerTutorial/src/main/GamePanel.java
+++ PlatformerTutorial/src/main/GamePanel.java
@@ -1,6 +1,12 @@
 package main;
 
+import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 import inputs.KeyboardInputs;
 import inputs.MouseInputs;
@@ -10,14 +16,33 @@ public class GamePanel extends JPanel{
 	private MouseInputs mouseInputs;
 	private int xDelta = 100;
 	private int yDelta = 100;
+	private BufferedImage img,subImg;
 	
 	public GamePanel() {
 		mouseInputs = new MouseInputs(this);
+		setPanelSize();
+		importImage();
+		
 		addKeyListener(new KeyboardInputs(this));
 		addMouseListener(mouseInputs);
 		addMouseMotionListener(mouseInputs);
 	}
 	
+	private void importImage() {
+		InputStream inputStream = getClass().getResourceAsStream("/player_sprites.png");
+		try {
+			img = ImageIO.read(inputStream);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private void setPanelSize() {
+		Dimension size = new Dimension(1280,800);
+		setPreferredSize(size);
+	}
+
 	public void changeXDelta(int value) {
 		this.xDelta += value;
 	}
@@ -33,7 +58,9 @@ public class GamePanel extends JPanel{
 	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.fillRect(xDelta,yDelta, 200, 50);
+		
+		subImg = img.getSubimage(1*64,8*40,64,40);
+		g.drawImage(subImg, (int)xDelta, (int)yDelta, 128,80, null);
 	}
 
 }
--- PlatformerTutorial/src/main/GameWindow.java
+++ PlatformerTutorial/src/main/GameWindow.java
@@ -8,11 +8,13 @@ public class GameWindow {
 	
 	public GameWindow (GamePanel gamePanel) {
 		jFrame = new JFrame();
-		jFrame.setSize(400, 400);
 		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		jFrame.setLocationRelativeTo(null);
+		//jFrame.setLocationRelativeTo(null);
 		
 		jFrame.add(gamePanel);
+		jFrame.setResizable(false);
+		
+		jFrame.pack();
 		
 		jFrame.setVisible(true);
 	}
